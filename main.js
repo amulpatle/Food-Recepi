f9ce685a8b15e1431b4b9ad009b65970d177edfc
amul patle <amulpatle3@gmail.com>
2023-06-01 01:30:19 +0530

    fetch data from api

---
+++ main.js
@@ -0,0 +1,17 @@
+const searchBox = document.querySelector(".searchBox");
+const searchBtn = document.querySelector(".searchBtn");
+const recipeContainer = document.querySelector(".recipe-container");
+
+const fetchRecipes = async (query) => {
+  const data =
+    await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}
+`);
+  const response = await data.json();
+  console.log(response.meals[0]);
+};
+
+searchBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const searchInput = searchBox.value.trim();
+  fetchRecipes(searchInput);
+});
