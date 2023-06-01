d40ef4e4e7635b28fc829fa91a40a0747f67713d
amul patle <amulpatle3@gmail.com>
2023-06-02 02:48:44 +0530

    add recipe Button

--- main.js
+++ main.js
@@ -3,13 +3,28 @@ const searchBtn = document.querySelector(".searchBtn");
 const recipeContainer = document.querySelector(".recipe-container");
 
 const fetchRecipes = async (query) => {
+  recipeContainer.innerHTML = "<h2>Fetching Recipes....</h2>";
   const data =
     await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}
 `);
   const response = await data.json();
 
+  recipeContainer.innerHTML = "";
   response.meals.forEach((meal) => {
-    console.log(meal);
+    // console.log(meal);
+    const recipeDiv = document.createElement("div");
+    recipeDiv.classList.add("recipe");
+    recipeDiv.innerHTML = `
+    <img src = "${meal.strMealThumb}">
+    <h3>${meal.strMeal}</h3>
+    <p><span>${meal.strArea}</span> Dish</p>
+    <p>Belongs to <span>${meal.strCategory}</span> Category</p>
+    `;
+
+    const button = document.createElement("button");
+    button.textContent = "view Recipe";
+    recipeDiv.appendChild(button);
+    recipeContainer.appendChild(recipeDiv);
   });
   // console.log(response.meals[0]);
 };
